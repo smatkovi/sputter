ee113c79df100078f48e946b199b5650a7957777
sebastian <sebastian.matkovich@gmail.com>
2018-09-21 01:15:08 +0200

    first really working commit

--- reactiveSputter.c
+++ reactiveSputter.c
@@ -5,7 +5,7 @@ int main()
   {
     FILE *inputvalues;
     inputvalues = fopen("input.dat", "r");
-    int i, n=1; //i...loop variable n...number of iterations
+    int i, n=200; //i...loop variable n...number of iterations
     double Pi = 4.0*atan(1), e = 1.6021766e-19, k_B = 1.38065e-23; //constants
     double p_N, T, M, S_N, J, A_t, A_c, S_M, S, alpha_t, alpha_c; //inputvalues
     double F, theta_one, theta_two, q_t, q_c, q_p, q_zero, R; //outputvalues
@@ -18,7 +18,7 @@ int main()
     printf("T is set to: %leK\n", T);
     printf("M is set to: %lekg\n", M);
     printf("S_N is set to: %le\n", S_N);
-    printf("J is set to: %lemA/cm²\n", J);
+    printf("J is set to: %leA/cm²\n", J);
     printf("A_t is set to: %lecm²\n", A_t);
     printf("A_c is set to: %lecm²\n", A_c);
     printf("S_M is set to: %le\n", S_M);
@@ -28,7 +28,7 @@ int main()
 
     fclose(inputvalues);
 
-    for(i=1; i==n; i++)
+    for(i=1; i<=n; i++)
       {
         //equation A5 for flux F of nitrogen molecules:
         F = p_N/sqrt(2.0*Pi*k_B*T*M);         
@@ -53,7 +53,7 @@ int main()
 	//equation A6 for total sputtering rate R:
         R = (J/e)*(S_N*theta_one + S_M*(1 - theta_one)); 	
 	
-	p_N+=0.01;
+	p_N+=0.1;
       }
 
     printf("values after %d times:\nF=%le\ntheta_1=%le\ntheta_2=%le\nq_t=%le\nq_c=%le\nq_p=%le\nq_zero=%le\nR=%le\n", n, F, theta_one, theta_two, q_t, q_c, q_p, q_zero, R);
