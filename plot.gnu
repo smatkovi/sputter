3f104070bbdb7550f455be112cd0e59b68154bd3
sebastian <sebastian.matkovich@gmail.com>
2018-09-21 16:46:44 +0200

    with plot

---
+++ plot.gnu
@@ -0,0 +1,3 @@
+se term postscript eps enhanced color
+se output "ROverq_0.ps"
+plot "output.dat" u 7:6 w l 
--- reactiveSputter.c
+++ reactiveSputter.c
@@ -9,11 +9,13 @@ int main()
     double Pi = 4.0*atan(1), e = 1.6021766e-19, k_B = 1.38065e-23; //constants
     double p_N, T, M, S_N, J, A_t, A_c, S_M, S, alpha_t, alpha_c; //inputvalues
     double F, theta_one, theta_two, q_t, q_c, q_p, q_zero, R; //outputvalues
+    double Fs[2*n], theta_ones[2*n], theta_twos[2*n], q_ts[2*n], q_cs[2*n], q_ps[2*n], q_zeros[2*n], Rs[2*n]; //storage arrays for outputvalues
 
-
-    if (inputvalues == NULL) printf ("file input.dat not found\n");
+    //read the inputvalues:
+    if (inputvalues == NULL) printf ("file input.dat could not be opened\n");
     else fscanf(inputvalues, "%lf %*s %lf %*s %lf %*s %lf %*s %lf %*s %lf %*s %lf %*s %lf %*s %lf %*s %lf %*s %lf %*s", &p_N, &T, &M, &S_N, &J, &A_t, &A_c, &S_M, &S, &alpha_t, &alpha_c);
     
+    //print initial inputvalues:
     printf("p_N is set to: %lebar\n", p_N);
     printf("T is set to: %leK\n", T);
     printf("M is set to: %lekg\n", M);
@@ -28,7 +30,7 @@ int main()
 
     fclose(inputvalues);
 
-    for(i=1; i<=n; i++)
+    for(i=0; i<2*n; i++)
       {
         //equation A5 for flux F of nitrogen molecules:
         F = p_N/sqrt(2.0*Pi*k_B*T*M);         
@@ -37,12 +39,12 @@ int main()
         theta_one = 1.0/(1.0 + J*S_N/(2.0*e*alpha_t*F));         
 	
 	//equation A2' for chamber wall and substrate fractional coverage theta_2:
-        theta_two = (e/J*2.0*alpha_c*F*A_c/A_t + S_N*theta_one)/(e/J*2.0*alpha_c*F*A_c/A_t + S_N*theta_one + S_M*(1 - theta_one));        
+        theta_two = (e/J*2.0*alpha_c*F*A_c/A_t + S_N*theta_one)/(e/J*2.0*alpha_c*F*A_c/A_t + S_N*theta_one + S_M*(1.0 - theta_one));        
 	//equation A3 for flow to target:
-        q_t = alpha_t*F*(1 - theta_one)*A_t;         
+        q_t = alpha_t*F*(1.0 - theta_one)*A_t;         
 	
 	//equation A4 for flow to surface area A_c of the chamber:
-        q_c = alpha_c*F*(1 - theta_two)*A_c;       
+        q_c = alpha_c*F*(1.0 - theta_two)*A_c;       
         
 	//equation 2 for flow through pump:
         q_p = p_N*S;        
@@ -52,10 +54,32 @@ int main()
 	
 	//equation A6 for total sputtering rate R:
         R = (J/e)*(S_N*theta_one + S_M*(1 - theta_one)); 	
-	
-	p_N+=0.1;
+
+	//store values in arrays:
+        Fs[i] = F;
+        theta_ones[i] = theta_one;
+        theta_twos[i] = theta_two;
+        q_ts[i] = q_t;
+        q_cs[i] = q_c;
+        q_ps[i] = q_p;
+        q_zeros[i] = q_zero;
+        Rs[i] = R;
+
+	if(i<n) p_N+=0.1;
+	else p_N-=0.1;
       }
 
+    //write calculated values to file:
+    FILE *outputvalues = fopen("output.dat","w");
+    if (outputvalues == NULL) printf ("file output.dat could not be opened\n");
+    fprintf(outputvalues, "F theta_1 theta_2 q_ts q_cs q_ps q_zero R\n");
+    for(i=0; i<2*n; i++)
+      {
+        fprintf(outputvalues, "%le %le %le %le %le %le %le %le\n", Fs[i], theta_ones[i], theta_twos[i], q_ts[i], q_cs[i], q_ps[i], q_zeros[i], Rs[i]);
+      }
+    fclose(outputvalues);
+
+    //print last calculated values:
     printf("values after %d times:\nF=%le\ntheta_1=%le\ntheta_2=%le\nq_t=%le\nq_c=%le\nq_p=%le\nq_zero=%le\nR=%le\n", n, F, theta_one, theta_two, q_t, q_c, q_p, q_zero, R);
 
     return 0;
